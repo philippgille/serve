a3a7d38a5ec9779321e4bd8bc1994b59904db683
Philipp Gille <philipp.gille@gmail.com>
2018-05-21 13:32:22 +0200

    Improve Snap packaging

--- build/build-snap-with-docker.sh
+++ build/build-snap-with-docker.sh
@@ -5,3 +5,6 @@ set -euxo pipefail
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run --rm -v ${SCRIPTDIR}/../:/build/serve -w /build/serve snapcraft/xenial-amd64 snapcraft
+
+VERSION=$(<$SCRIPTDIR/../VERSION)
+mv "${SCRIPTDIR}/../serve_${VERSION}_amd64.snap" "${SCRIPTDIR}/artifacts"
